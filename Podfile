1adca7f79407abd19906f85f46b774ad1f15c5a1
JustinGuedes <justin.guedes@gmail.com>
2016-09-04 18:13:42 +0200

    Add project files and setup with Cocoapods

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Ramda' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Ramda
+
+  target 'RamdaTests' do
+    inherit! :search_paths
+    # Pods for testing
+
+    pod 'Fox', '1.0.1'
+
+  end
+
+  target 'RamdaUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Ramda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ramda
+//
+//  Created by Justin Guedes on 2016/09/04.
+//  Copyright © 2016 NPO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
