a4b0fa56bb23e60aad9e4bab15cd49999a67033e
TYRONE AVNIT <tyrone.avnit@standardbank.co.za>
2016-09-06 16:06:00 +0200

    Add all extenstion

---
+++ Ramda/Extensions/all.swift
@@ -0,0 +1,24 @@
+//
+// Created by TYRONE AVNIT on 2016/09/06.
+//
+
+import Foundation
+
+extension R {
+
+    public class func all<T>(function: (T) -> Bool) -> (array: [T]) -> Bool {
+        return { array in
+            for element in array {
+                if(!function(element)) {
+                    return false
+                }
+            }
+            return true
+        }
+    }
+
+    public class func all<T>(function: (T) -> Bool, array: [T]) -> Bool {
+        return all(function)(array: array)
+    }
+
+}
