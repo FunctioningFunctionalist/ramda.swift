98468d12d55477b03f70a90c0101af8d2335cd2a
JustinGuedes <justin.guedes@gmail.com>
2016-09-08 18:33:09 +0200

    Add and function that compares two conditions

---
+++ Example/Tests/Extensions/AndTests.swift
@@ -0,0 +1,152 @@
+//
+//  AndTests.swift
+//  Ramda
+//
+//  Created by Justin Guedes on 2016/09/08.
+//  Copyright (c) 2016 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import Fox
+import Ramda
+
+class AndTests: XCTestCase {
+
+    func testShouldAndBothFunctionsAndProduceCorrectResultWithPartialFunctions() {
+
+        let partialGenerator = FoxGeneratorParam1(FOXBoolean()) {
+
+            (lhs: Bool) in
+
+            let lhsFunc = { return lhs }
+
+            let partial = R.and(lhsFunc)
+
+            let booleanGenerator = FoxGeneratorParam1(FOXBoolean()) {
+
+                (rhs: Bool) in
+
+                let rhsFunc = { return rhs }
+
+                let result = partial(rhs: rhsFunc)
+
+                return result == (lhs && rhs)
+
+            }
+
+            return FoxTester.assert(booleanGenerator)
+
+        }
+
+        FoxTester.assert(partialGenerator)
+
+    }
+
+    func testShouldAndBothFunctionsAndProduceCorrectResult() {
+
+        let booleanGenerator = FoxGeneratorParam2(FOXBoolean(), FOXBoolean()) {
+
+            (lhs: Bool, rhs: Bool) in
+
+            let lhsFunc = { return lhs }
+            let rhsFunc = { return rhs }
+
+            let result = R.and(lhsFunc, rhs: rhsFunc)
+
+            return result == (lhs && rhs)
+
+        }
+
+        FoxTester.assert(booleanGenerator)
+
+    }
+
+    func testShouldAndBothConditionsAndProduceCorrectResultWithPartialFunctions() {
+
+        let partialGenerator = FoxGeneratorParam1(FOXBoolean()) {
+
+            (lhs: Bool) in
+
+            let partial = R.and(lhs)
+
+            let booleanGenerator = FoxGeneratorParam1(FOXBoolean()) {
+
+                (rhs: Bool) in
+
+                let result = partial(rhs: rhs)
+
+                return result == (lhs && rhs)
+
+            }
+
+            return FoxTester.assert(booleanGenerator)
+
+        }
+
+        FoxTester.assert(partialGenerator)
+
+    }
+
+    func testShouldAndBothConditionsAndProduceCorrectResult() {
+
+        let booleanGenerator = FoxGeneratorParam2(FOXBoolean(), FOXBoolean()) {
+
+            (lhs: Bool, rhs: Bool) in
+
+            let result = R.and(lhs, rhs: rhs)
+
+            return result == (lhs && rhs)
+
+        }
+
+        FoxTester.assert(booleanGenerator)
+
+    }
+
+    func testShouldNotExecuteSecondConditionIfFirstConditionIsFalse() {
+
+        let lhsFunc: () -> Bool = {
+
+            return false
+
+        }
+
+        var executed = false
+        let rhsFunc: () -> Bool = {
+
+            executed = true
+            return true
+
+        }
+
+        let result = R.and(lhsFunc, rhs: rhsFunc)
+
+        XCTAssertFalse(result)
+        XCTAssertFalse(executed)
+
+    }
+
+    func testShouldExecuteSecondConditionIfFirstConditionIsTrue() {
+
+        let lhsFunc: () -> Bool = {
+
+            return true
+
+        }
+
+        var executed = false
+        let rhsFunc: () -> Bool = {
+
+            executed = true
+            return false
+
+        }
+
+        let result = R.and(lhsFunc, rhs: rhsFunc)
+
+        XCTAssertFalse(result)
+        XCTAssertTrue(executed)
+
+    }
+
+}
