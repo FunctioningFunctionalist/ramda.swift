b950c4fd7a7439c4b39749a3736bc93136c69221
JustinGuedes <justin.guedes@gmail.com>
2016-09-09 20:45:26 +0200

    Add ap function

---
+++ Ramda/Extensions/ap.swift
@@ -0,0 +1,64 @@
+//
+// Created by Justin Guedes on 2016/09/09.
+//
+
+import Foundation
+
+extension R {
+
+    /**
+
+        Applies a list of functions to a list of values.
+
+        - parameter functions: The functions to apply to each value.
+
+        - returns: An array of results of applying each of the functions to all of values in turn.
+
+    */
+
+    public class func ap<A, B>(functions: (A -> B)...) -> (array: [A]) -> [B] {
+        return { (array: [A]) in
+            functions.flatMap { (function: A -> B) in
+                array.map { (element: A) in
+                    function(element)
+                }
+            }
+        }
+    }
+
+    /**
+
+        Applies a list of functions to a list of values.
+
+        - parameter functions: The functions to apply to each value.
+
+        - returns: An array of results of applying each of the functions to all of values in turn.
+
+    */
+
+    public class func ap<A, B>(functions: [A -> B]) -> (array: [A]) -> [B] {
+        return { (array: [A]) in
+            functions.flatMap { (function: A -> B) in
+                array.map { (element: A) in
+                    function(element)
+                }
+            }
+        }
+    }
+
+    /**
+
+       Applies a list of functions to a list of values.
+
+        - parameter functions: An array of functions to apply to each value.
+        - parameter array: An array of values.
+
+        - returns: An array of results of applying each of the functions to all of values in turn.
+
+    */
+
+    public class func ap<A, B>(functions: [A -> B], array: [A]) -> [B] {
+        return ap(functions)(array: array)
+    }
+
+}
