1d4d945eba893c6fcafef3a5affd3cc87e4e2ac7
JustinGuedes <justin.guedes@gmail.com>
2016-09-11 19:40:50 +0200

    Add subtract function

---
+++ Example/Tests/Extensions/SubtractTests.swift
@@ -0,0 +1,55 @@
+//
+//  SubtractTests.swift
+//  Ramda
+//
+//  Created by Justin Guedes on 2016/09/11.
+//  Copyright (c) 2016 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import Fox
+import Ramda
+
+// swiftlint:disable line_length
+
+class SubtractTests: XCTestCase {
+
+    func testShouldSubtractTwoIntegers() {
+        let integerGenerator = FoxGeneratorParam2(FOXInteger(), FOXInteger()) { (number1: Int, number2: Int) in
+            let expectedResult = number1 - number2
+
+            let result = R.subtract(number2, from: number1)
+
+            return expectedResult == result
+        }
+
+        FoxTester.assert(integerGenerator)
+    }
+
+    func testShouldSubtractTwoFloats() {
+        let floatGenerator = FoxGeneratorParam2(FOXFloat(), FOXFloat()) { (number1: Float, number2: Float) in
+            let expectedResult = number1 - number2
+
+            let result = R.subtract(number2, from: number1)
+
+            return expectedResult == result
+        }
+
+        FoxTester.assert(floatGenerator)
+    }
+
+    func testShouldSubtractTwoDoubles() {
+        let doubleGenerator = FoxGeneratorParam2(FOXDouble(), FOXDouble()) { (number1: Double, number2: Double) in
+            let expectedResult = number1 - number2
+
+            let result = R.subtract(number2, from: number1)
+
+            return expectedResult == result
+        }
+
+        FoxTester.assert(doubleGenerator)
+    }
+
+}
+
+// swiftlint:enable line_length
---
+++ Ramda/Extensions/subtract.swift
@@ -0,0 +1,73 @@
+//
+// Created by Justin Guedes on 2016/09/11.
+//
+
+import Foundation
+
+extension R {
+
+    /**
+
+        Subtracts first argument from second.
+
+        - parameter lhs: The left hand side operand.
+        - parameter rhs: The right hand side operand.
+
+        - returns: Returns the difference between the 'rhs' and the 'lhs'.
+
+    */
+
+    public class func subtract<T: SubtractionArithmeticType>(lhs: T, from rhs: T) -> T {
+        return rhs - lhs
+    }
+
+    /**
+
+        Subtracts first argument from second.
+
+        - parameter lhs: The left hand side operand.
+
+        - returns: Returns the difference between the 'rhs' and the 'lhs'.
+
+    */
+
+    public class func subtract<T: SubtractionArithmeticType>(lhs: T) -> (from: T) -> T {
+        return curry(subtract)(lhs)
+    }
+
+}
+
+/**
+
+ Protocol for types that support subtraction arithmetic.
+
+*/
+
+public protocol SubtractionArithmeticType {
+
+    /**
+
+        Subtracts "lhs" and "rhs", returning a result of same type.
+
+        - parameter lhs: The left hand side operand.
+        - parameter rhs: The right hand side operand.
+
+        - returns: The difference of the two values.
+
+    */
+
+    func - (lhs: Self, rhs: Self) -> Self
+}
+
+extension Int : SubtractionArithmeticType {}
+extension Int8 : SubtractionArithmeticType {}
+extension Int16 : SubtractionArithmeticType {}
+extension Int32 : SubtractionArithmeticType {}
+extension Int64 : SubtractionArithmeticType {}
+extension UInt : SubtractionArithmeticType {}
+extension UInt8 : SubtractionArithmeticType {}
+extension UInt16 : SubtractionArithmeticType {}
+extension UInt32 : SubtractionArithmeticType {}
+extension UInt64 : SubtractionArithmeticType {}
+extension Float : SubtractionArithmeticType {}
+extension Double : SubtractionArithmeticType {}
