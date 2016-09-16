16433c96ca2afe79fc4d6baa00186739b60c800a
JustinGuedes <justin.guedes@gmail.com>
2016-09-16 21:07:57 +0200

    Add not function

---
+++ Ramda/Extensions/not.swift
@@ -0,0 +1,23 @@
+//
+// Created by Justin Guedes on 2016/09/16.
+//
+
+import Foundation
+
+extension R {
+
+    /**
+
+        A function that returns the ! or its argument.
+
+        - parameter value: Value to negate
+
+        - returns: True if value is false, otherwise false.
+
+    */
+
+    public class func not(value: Bool) -> Bool {
+        return !value
+    }
+
+}
