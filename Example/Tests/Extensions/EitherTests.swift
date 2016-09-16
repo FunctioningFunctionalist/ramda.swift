21f184b5433a7a8eda788fd8ffc9db149da1c63e
JustinGuedes <justin.guedes@gmail.com>
2016-09-16 18:56:09 +0200

    Add either function

---
+++ Example/Tests/Extensions/EitherTests.swift
@@ -0,0 +1,68 @@
+//
+//  EitherTests.swift
+//  Ramda
+//
+//  Created by Justin Guedes on 2016/09/16.
+//  Copyright (c) 2016 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import Ramda
+
+class EitherTests: XCTestCase {
+
+    func testShouldReturnTrueAndNotExecuteTheSecondFunctionWhenTheFirstPredicateIsTrue() {
+        var executed = false
+        let isEven: Int -> Bool = {
+            return $0 % 2 == 0
+        }
+
+        let isGreaterThan10: Int -> Bool = {
+            executed = true
+            return $0 > 10
+        }
+
+        let either = R.either(isEven)(or: isGreaterThan10)
+        let result = either(12)
+
+        XCTAssertTrue(result)
+        XCTAssertFalse(executed)
+    }
+
+    func testShouldReturnTrueAndExecuteBothPredicatesWhenFirstPredicateIsFalse() {
+        var executed = false
+        let isEven: Int -> Bool = {
+            return $0 % 2 == 0
+        }
+
+        let isGreaterThan10: Int -> Bool = {
+            executed = true
+            return $0 > 10
+        }
+
+        let either = R.either(isEven)(or: isGreaterThan10)
+        let result = either(13)
+
+        XCTAssertTrue(result)
+        XCTAssertTrue(executed)
+    }
+
+    func testShouldReturnFalseWhenBothPredicatesResultInFalse() {
+        var executed = false
+        let isEven: Int -> Bool = {
+            return $0 % 2 == 0
+        }
+
+        let isGreaterThan10: Int -> Bool = {
+            executed = true
+            return $0 > 10
+        }
+
+        let either = R.either(isEven)(or: isGreaterThan10)
+        let result = either(7)
+
+        XCTAssertFalse(result)
+        XCTAssertTrue(executed)
+    }
+
+}
---
+++ Ramda/Extensions/either.swift
@@ -0,0 +1,43 @@
+//
+// Created by Justin Guedes on 2016/09/16.
+//
+
+import Foundation
+
+extension R {
+
+    /**
+
+        A function wrapping calls to the two functions in an || operation. Note this function
+        is short-circuited.
+
+        - parameter lhs: The first predicate.
+        - parameter rhs: The second predicate.
+
+        - returns: True if the first predicate is true or the second, else false.
+
+    */
+
+    public class func either<T>(lhs: (T) -> Bool, or rhs: (T) -> Bool) -> (T) -> Bool {
+        return { element in
+            return lhs(element) || rhs(element)
+        }
+    }
+
+    /**
+
+        A function wrapping calls to the two functions in an || operation. Note this function
+        is short-circuited.
+
+        - parameter lhs: The first predicate.
+
+        - returns: A curried function that accepts the second predicate and returns true if
+        the first predicate is true or the second, else false.
+
+    */
+
+    public class func either<T>(lhs: (T) -> Bool) -> (or: (T) -> Bool) -> (T) -> Bool {
+        return curry(either)(lhs)
+    }
+
+}
