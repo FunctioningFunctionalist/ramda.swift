b7c6824df6c10a60542fc0ce42237bf45b2a90b6
JustinGuedes <justin.guedes@gmail.com>
2016-09-22 18:28:36 +0200

    Add insert function

---
+++ Example/Tests/Extensions/InsertTests.swift
@@ -0,0 +1,32 @@
+//
+//  InsertTests.swift
+//  Ramda
+//
+//  Created by Justin Guedes on 2016/09/22.
+//  Copyright (c) 2016 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import Ramda
+
+class InsertTests: XCTestCase {
+
+    func testShouldInsertElementIntoArray() {
+        let expectedResult = [1, 2, 3, 5]
+
+        let result = R.insert(1)(with: 2)(in: [1, 3, 5])
+
+        XCTAssertEqual(expectedResult, result)
+    }
+
+    func testShouldReturnNewCollectionWithInsertedElementAndNotAffectOriginalCollection() {
+        let expectedResult = [1, 3, 5, 6, 7, 9]
+        let input = [1, 3, 5, 7, 9]
+
+        let result = R.insert(3)(with: 6)(in: input)
+
+        XCTAssertEqual(expectedResult, result)
+        XCTAssertEqual(input, [1, 3, 5, 7, 9])
+    }
+
+}
