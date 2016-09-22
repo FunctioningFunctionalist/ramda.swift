83bb7de2fe24a481399559c2a2e4d4250e59cd51
JustinGuedes <justin.guedes@gmail.com>
2016-09-22 19:19:30 +0200

    Add minBy function

--- Ramda/Extensions/minBy.swift
+++ Ramda/Extensions/minBy.swift
@@ -9,34 +9,34 @@ extension R {
     /**
 
         Takes a function and two values, and returns whichever value
-        produces the larger result when passed to the provided function.
+        produces the smaller result when passed to the provided function.
 
         - parameter function: The function to create the element to compare.
         - parameter lhs: The first input for the function.
         - parameter rhs: The second input for the function.
 
-        - returns: The larger element.
+        - returns: The smaller element.
 
     */
 
-    public class func maxBy<A, B: Comparable>(function: A -> B, with lhs: A, or rhs: A) -> B {
-        return R.max(function(lhs), or: function(rhs))
+    public class func minBy<A, B: Comparable>(function: A -> B, with lhs: A, or rhs: A) -> B {
+        return R.min(function(lhs), or: function(rhs))
     }
 
     /**
 
         Takes a function and two values, and returns whichever value
-        produces the larger result when passed to the provided function.
+        produces the smaller result when passed to the provided function.
 
         - parameter function: The function to create the element to compare.
 
         - returns: A curried function that accepts the first and second element to
-        return the larger element.
+        return the smaller element.
 
     */
 
-    public class func maxBy<A, B: Comparable>(function: A -> B) -> (with: A) -> (or: A) -> B {
-        return curry(maxBy)(function)
+    public class func minBy<A, B: Comparable>(function: A -> B) -> (with: A) -> (or: A) -> B {
+        return curry(minBy)(function)
     }
 
 }
