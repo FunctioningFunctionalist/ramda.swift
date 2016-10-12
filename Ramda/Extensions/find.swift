a18c2c079c26fa351c32457546fddae65b236297
TYRONEMICHAEL <tyronemichael@gmail.com>
2016-10-12 17:48:53 +0200

    Fix swiftlint warnings

--- Ramda/Extensions/find.swift
+++ Ramda/Extensions/find.swift
@@ -7,7 +7,7 @@ import Foundation
 // swiftlint:disable line_length
 
 extension R {
-    
+
     /**
      Returns the first element of the list which matches the predicate,
      or nil if no element matches.
@@ -15,7 +15,7 @@ extension R {
      - parameter sequence: The sequence to check.
      - returns: The first found element, or nil if none found.
      */
-    
+
     public class func find<A, B: SequenceType where A == B.Generator.Element>(predicate: (A) -> Bool, in sequence: B) -> A? {
         for element in sequence {
             if predicate(element) {
@@ -24,7 +24,7 @@ extension R {
         }
         return nil
     }
-    
+
     /**
      Returns the first element of the list which matches the predicate,
      or nil if no element matches.
@@ -32,9 +32,9 @@ extension R {
      - returns: A curried function that accepts a sequence and returns
      the first found element, or nil if none found.
      */
-    
+
     public class func find<A, B: SequenceType where A == B.Generator.Element>(predicate: (A) -> Bool) -> (in: B) -> A? {
         return curry(find)(predicate)
     }
-    
+
 }
