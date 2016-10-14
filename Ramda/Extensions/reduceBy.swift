3d75313eeecc0eeb26f2fa275ea84c3e85f5a268
TYRONEMICHAEL <tyronemichael@gmail.com>
2016-10-14 15:44:33 +0200

    Complete extension reduceBy with tests

--- Ramda/Extensions/reduceBy.swift
+++ Ramda/Extensions/reduceBy.swift
@@ -12,21 +12,64 @@ import Foundation
 
 extension R {
 
-    public class func reduceBy<A, B: SequenceType, C, D: SequenceType where A == B.Generator.Element, C == D.Generator.Element>
-        (valueFn: (B, A) -> D, valueAcc: B, keyFn: A -> String, list: B) -> [String: D] {
-        
-        typealias ReduceSignature = (startingWith: [String: D]) -> (in: B) -> [String: D]
+    /**
 
-        let reduceBy: ReduceSignature = R.reduce { (acc, elment) in
+     Groups the elements of the list according to the result of calling the
+     String-returning function keyFn on each element and reduces the elements
+     of each group to a single value via the reducer function valueFn. This function
+     is basically a more general groupBy function. Acts as a transducer if a
+     transformer is given in list position.
+
+     - parameter valueFn: The function that produces accumulator result.
+     - parameter valueAcc: The initial value for accumulating.
+     - parameter keyFn: The function that maps the list's element into a key.
+     - parameter list: The sequence to reduce.
+
+     - returns: An object with the output of keyFn for keys, mapped to the output of
+     valueFn for elements which produced that key when passed to keyFn.
+
+     */
+
+    public class func reduceBy<A, B: SequenceType, C: Hashable, D: SequenceType where A == D.Generator.Element>
+        (valueFn: (B, A) -> B, valueAcc: B, keyFn: A -> C, list: D) -> [C: B] {
+
+        typealias ReduceSignature = (startingWith: [C: B]) -> (in: D) -> [C: B]
+
+        let reduceBy: ReduceSignature = R.reduce { (acc, element) in
             var result = acc
-            let key = keyFn(elment)
-            result[key] = valueFn(valueAcc, elment)
+            let key = keyFn(element)
+
+            if let keyValue = result[key] {
+                result[key] = valueFn(keyValue, element)
+                return result
+            }
+
+            result[key] = valueFn(valueAcc, element)
             return result
         }
 
-        return reduceBy(startingWith: [String: D]())(in: list)
+        return reduceBy(startingWith: [C: B]())(in: list)
     }
 
+    /**
+
+     Groups the elements of the list according to the result of calling the
+     String-returning function keyFn on each element and reduces the elements
+     of each group to a single value via the reducer function valueFn. This function
+     is basically a more general groupBy function. Acts as a transducer if a
+     transformer is given in list position.
+
+     - parameter valueFn: The function that produces accumulator result.
+
+     - returns: A curried function that accepts the initial value and sequence and
+     returns the keyFn.
+
+     */
+
+    public class func reduceBy<A, B: SequenceType, C: Hashable, D: SequenceType where A == D.Generator.Element>
+        (valueFn: (B, A) -> B) -> (valueAcc: B) -> (keyFn: A -> C) -> (list: D) -> [C: B] {
+        return curry(reduceBy)(valueFn)
+    }
 }
 
 // swiftlint:enable line_length
