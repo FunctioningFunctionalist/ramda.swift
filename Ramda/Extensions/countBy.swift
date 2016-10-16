a90d806c7353181f1e6e00584afa1347fff94098
TYRONEMICHAEL <tyronemichael@gmail.com>
2016-10-16 21:48:34 +0200

    Add countBy extension

--- Ramda/Extensions/countBy.swift
+++ Ramda/Extensions/countBy.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+// swiftlint:disable line_length
+
+extension R {
+
+    /**
+
+     Groups the elements of the list according to the result of calling the
+     String-returning function keyFn on each element and reduces the elements
+     of each group to a single value via the reducer function valueFn. This function
+     is basically a more general groupBy function. Acts as a transducer if a
+     transformer is given in list position.
+
+     - parameter functionForKeys: The function that maps the list's element into a key.
+
+     - returns: An object with the output of keyFn for keys, mapped to the output of
+     valueFn for elements which produced that key when passed to keyFn.
+
+     */
+
+    public class func countBy<B: SequenceType, C: Hashable>(functionForKeys: B.Generator.Element -> C) -> (in: B) -> [C: Int] {
+        typealias Signature = ((B.Generator.Element) -> C) -> (B) -> [C: Int]
+        let result: Signature = R.reduceBy({ (acc, element) in acc + 1 })(startingWith: 0)
+        return result(functionForKeys)
+    }
+}
+
+// swiftlint:enable line_length
