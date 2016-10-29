d726156b9b456cb7e440d136e32b666e8288fedf
TYRONEMICHAEL <tyronemichael@gmail.com>
2016-10-29 07:14:22 +0200

    Add extension comparator

---
+++ Ramda/Extensions/comparator.swift
@@ -0,0 +1,49 @@
+//
+//  comparator.swift
+//  Pods
+//
+//  Created by TYRONE AVNIT on 10/25/16.
+//
+//
+
+import Foundation
+
+// swiftlint:disable line_length
+
+extension R {
+
+    /**
+
+     Makes a comparator function out of a function that reports whether the first
+     element is less than the second.
+
+     - parameter predicate: A predicate function of arity two.
+
+     - returns: A Function :: a -> b -> Int that returns -1 if a < b, 1 if b < a, otherwise 0
+
+     */
+
+    public class func comparator<A>(predicate: @escaping (A, A) -> Bool) -> (_ firstElement: A, _ secondElement: A) -> Int {
+        return { firstElement, secondElement in
+            predicate(firstElement, secondElement) ? -1 : predicate(secondElement, firstElement) ? 1 : 0
+        }
+    }
+
+    /**
+
+     Makes a comparator function out of a function that reports whether the first
+     element is less than the second.
+
+     - parameter predicate: A predicate function of arity two.
+
+     - returns: A curried function that accepts the first Element as a paramerter
+
+     */
+
+    public class func comparator<A>(predicate: @escaping (A, A) -> Bool) -> (_ firstElement: A) -> (_ secondElement: A) -> Int {
+        return curry(comparator(predicate: predicate))
+    }
+
+}
+
+// swiftlint:enable line_length
