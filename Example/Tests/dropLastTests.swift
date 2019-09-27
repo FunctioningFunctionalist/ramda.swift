d247169dbb45d87bd95b08d824325c8a2b9dd531
Christian Ray Leovido <christian.leovido@gmail.com>
2019-09-27 21:25:54 +0100

    add dropLast + tests

---
+++ Example/Tests/dropLastTests.swift
@@ -0,0 +1,32 @@
+import Foundation
+import XCTest
+import Ramda
+
+class DropLastTests: XCTestCase {
+
+    func testDropLastString() {
+        let result = R.dropLast(3, "ramda")
+        XCTAssertEqual(result, "ra")
+    }
+
+    func testDropLastString_BeyondRange() {
+        let result = R.dropLast(1000, "ramda")
+        XCTAssertEqual(result, "")
+    }
+
+    func testDropLastArrayString_First() {
+        let result = R.dropLast(1, ["foo", "bar", "baz"])
+        XCTAssertEqual(result, ["foo", "bar"])
+    }
+
+    func testDropLastArrayFirstString_FirstAndSecond() {
+        let result = R.dropLast(2, ["foo", "bar", "baz"])
+        XCTAssertEqual(result, ["foo"])
+    }
+
+    func testDropLastArrayFirstString_All() {
+        let result = R.dropLast(3, ["foo", "bar", "baz"])
+        XCTAssertEqual(result, [])
+    }
+
+}
---
+++ Ramda/Extensions/dropLast.swift
@@ -0,0 +1,49 @@
+//
+//  dropLast.swift
+//  Ramda
+//
+//  Created by Christian Leovido on 27/09/2019.
+//
+
+import Foundation
+
+extension R {
+
+    /**
+
+     Returns all but the first n elements
+     of the given list, string, or transducer/transformer (or object with a drop method).
+
+     Dispatches to the drop method of the second argument, if present.
+
+     */
+
+    public class func dropLast(_ iteration: Int, _ string: String) -> String {
+
+        var newString = string
+
+        for _ in 0..<iteration {
+            if newString.count == 0 {
+                break
+            } else {
+                newString.removeLast()
+            }
+        }
+
+        return newString
+    }
+
+    public class func dropLast(_ iteration: Int, _ arrayString: [String]) -> [String] {
+
+        var newArrayString = arrayString
+
+        for index in 0..<iteration where arrayString.count > index {
+
+            newArrayString.removeLast()
+
+        }
+
+        return newArrayString
+    }
+
+}
