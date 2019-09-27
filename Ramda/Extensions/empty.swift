b4b02146ea5ad1e3e1b7b474b8ed0a2b243183ad
Christian Ray Leovido <christian.leovido@gmail.com>
2019-09-27 17:59:17 +0100

    add empty and emptyTests

---
+++ Ramda/Extensions/empty.swift
@@ -0,0 +1,85 @@
+//
+//  empty.swift
+//  Ramda
+//
+//  Created by Christian Leovido on 27/09/2019.
+//
+
+import Foundation
+
+public protocol EmptyDefinable {
+
+    associatedtype CollectionType: Collection
+    associatedtype SubCollectionType: Collection
+
+    mutating func emptyCollection() -> CollectionType
+}
+
+extension R {
+
+    /**
+
+     Returns the empty value of its argument's type.
+
+     - parameter value: must conform to the EmptyDefinable protocol
+
+     - returns: an empty collection type (Array, Dictionary, Set, String)
+
+     */
+
+    public class func empty<T: EmptyDefinable>(value: T) -> T? {
+        var incomingEmptyDefinable = value
+
+        return incomingEmptyDefinable.emptyCollection() as? T
+    }
+
+}
+
+extension Array: EmptyDefinable {
+
+    public typealias CollectionType = [Any]
+    public typealias SubCollectionType = CollectionType.SubSequence
+
+    mutating public func emptyCollection() -> CollectionType {
+        self.removeAll()
+
+        return self
+    }
+
+}
+
+extension Dictionary: EmptyDefinable {
+
+    public typealias CollectionType = [AnyHashable: Any]
+    public typealias SubCollectionType = CollectionType.SubSequence
+
+    mutating public func emptyCollection() -> CollectionType {
+        self.removeAll()
+
+        return self
+    }
+}
+
+extension String: EmptyDefinable {
+
+    public typealias CollectionType = String
+    public typealias SubCollectionType = CollectionType.SubSequence
+
+    mutating public func emptyCollection() -> CollectionType {
+        self.removeAll()
+
+        return self
+    }
+}
+
+extension Set: EmptyDefinable {
+
+    public typealias CollectionType = Set<AnyHashable>
+    public typealias SubCollectionType = CollectionType.SubSequence
+
+    mutating public func emptyCollection() -> CollectionType {
+        self.removeAll()
+
+        return self
+    }
+}
