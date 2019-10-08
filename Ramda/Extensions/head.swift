611c91696771dcb6c0e194d00db54162e648e0df
Christian Ray Leovido <christian.leovido@gmail.com>
2019-10-08 21:46:18 +0100

    guard value as opposed to force unwrapping

--- Ramda/Extensions/head.swift
+++ Ramda/Extensions/head.swift
@@ -19,18 +19,24 @@ extension R {
 
      */
     public class func head(_ string: String) -> String {
-        return string.first == nil ? "" : String(string.first!)
+
+        guard let head = string.first else {
+            return ""
+        }
+
+        return String(head)
+
     }
 
     /**
 
-    Returns the first element of the given array of strings or nil
+     Returns the first element of the given array of strings or nil
 
-    - parameter arrayString: Array of strings to check
+     - parameter arrayString: Array of strings to check
 
-    - returns: The first element of the given array of strings or nil
+     - returns: The first element of the given array of strings or nil
 
-    */
+     */
     public class func head(_ arrayString: [String]) -> String? {
         return arrayString.first
     }
