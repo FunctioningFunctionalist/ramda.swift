afb47430cbf198d22f6a0acd03a0d52d685d5e41
Christian Ray Leovido <christian.leovido@gmail.com>
2020-04-17 23:44:55 +0100

    add toUpper + tests

---
+++ Ramda/Extensions/toUpper.swift
@@ -0,0 +1,22 @@
+//
+//  toUpper.swift
+//  Pods-Ramda_Example
+//
+//  Created by Christian Leovido on 17/04/2020.
+//
+
+extension R {
+
+    /// The lower case version of a string.
+    ///
+    /// See also toUpper.
+    ///
+    /// - Parameters:
+    ///   - text: The string to lower case.
+    ///
+    /// - Returns: The lower case version of `text`.
+    public class func toUpper(_ text: String) -> String {
+        return text.uppercased()
+    }
+
+}
